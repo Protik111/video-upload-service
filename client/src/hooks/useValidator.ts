910e551486ab279aa355d5e95cf75bf462667fb6
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-08-29 14:26:11 +0600

    [FIXED] hooks

--- client/src/hooks/useValidator.ts
+++ client/src/hooks/useValidator.ts
@@ -3,27 +3,37 @@ import { useState } from 'react'
 interface State {
   email?: string
   password?: string
+  [key: string]: string | undefined
+}
+interface CustomMessages {
+  [key: string]: string
+}
+
+interface ValidationResult {
+  error: State
+  isError: boolean
 }
 
 const useValidator = () => {
   const [errors, setErrors] = useState<State>({})
 
-  const camelCaseToCapitalized = (str: string) => {
+  const camelCaseToCapitalized = (str: string): string => {
     // Convert camelCase to space-separated words and capitalize each word
     return str
       .replace(/([A-Z])/g, ' $1')
-      .replace(/^./, function (str) {
-        return str.toUpperCase()
-      })
+      .replace(/^./, str => str.toUpperCase())
       .split(' ')
       .map(word => word.charAt(0).toUpperCase() + word.slice(1))
       .join(' ')
   }
 
-  const validate = (fields, customMessages = {}) => {
-    let error = {}
+  const validate = (
+    fields: State,
+    customMessages: CustomMessages = {},
+  ): ValidationResult => {
+    const error: State = {}
 
-    for (let key in fields) {
+    for (const key in fields) {
       if (!fields[key]) {
         const defaultMessage = `${camelCaseToCapitalized(key)} is required!`
         const message = customMessages[key] || defaultMessage
